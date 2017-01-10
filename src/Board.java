a76734fa9f76f58522e0cf72a033945a56db1f13
Mattijs Kuhlmann <matju.k@hotmail.com>
2017-01-10 10:44:28 +0100

    Nodes added

--- src/Board.java
+++ src/Board.java
@@ -7,18 +7,29 @@ public class Board {
 
     public void init(){
 
+        Node nodeOne = new Node(1, Colour.PURPLE);
+        Node nodeTwo = new Node(2, Colour.BLACK);
+        Node nodeThree = new Node(3, Colour.GREEN);
+        Node nodeFour = new Node(4, Colour.GREEN);
+        Node nodeFive = new Node(5, Colour.GREEN);
+        Node nodeSix = new Node(6, Colour.ORANGE);
+        Node nodeSeven = new Node(7, Colour.ORANGE);
+        Node nodeEight = new Node(8, Colour.PURPLE);
+        Node nodeNine = new Node(9, Colour.PURPLE);
+        Node nodeTen = new Node(10, Colour.BLACK);
+        Node nodeEleven = new Node(11, Colour.ORANGE);
         Node nodeTwelve = new Node(12,Colour.PURPLE);
-        Node nodeThirteen = new Node(13,Colour.PURPLE);
-        Node nodeFourteen = new Node(14,Colour.PURPLE);
-        Node nodeFifteen = new Node(15,Colour.PURPLE);
-        Node nodeSixteen = new Node(16,Colour.PURPLE);
-        Node nodeSeventeen = new Node(17,Colour.PURPLE);
-        Node nodeEighteen = new Node(18,Colour.PURPLE);
-        Node nodeNineteen = new Node(19,Colour.PURPLE);
-        Node nodeTwentee = new Node(20,Colour.PURPLE);
-        Node nodeTwenteeOne = new Node(21,Colour.PURPLE);
-        Node nodeTwenteeTwo = new Node(22,Colour.PURPLE);
-        Node nodeTwenteeThree = new Node(23,Colour.PURPLE);
+        Node nodeThirteen = new Node(13,Colour.ORANGE);
+        Node nodeFourteen = new Node(14,Colour.GREEN);
+        Node nodeFifteen = new Node(15,Colour.ORANGE);
+        Node nodeSixteen = new Node(16,Colour.GREEN);
+        Node nodeSeventeen = new Node(17,Colour.GREEN);
+        Node nodeEighteen = new Node(18,Colour.BLACK);
+        Node nodeNineteen = new Node(19,Colour.ORANGE);
+        Node nodeTwenty = new Node(20,Colour.GREEN);
+        Node nodeTwenteeOne = new Node(21,Colour.BLACK);
+        Node nodeTwenteeTwo = new Node(22,Colour.BLACK);
+        Node nodeTwenteeThree = new Node(23,Colour.BLUE);
 
 
     }
