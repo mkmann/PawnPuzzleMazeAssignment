c1b8d220deef8512267adf95c867be368d0b8728
madsl <mads.lundgaard@live.no>
2017-01-10 10:41:31 +0100

    nodes created from 12

--- src/Board.java
+++ src/Board.java
@@ -5,6 +5,22 @@ public class Board {
 
     public enum Colour {PURPLE, ORANGE, BLACK, GREEN, BLUE}
 
+    public void init(){
 
+        Node nodeTwelve = new Node(12,Colour.PURPLE);
+        Node nodeThirteen = new Node(13,Colour.PURPLE);
+        Node nodeFourteen = new Node(14,Colour.PURPLE);
+        Node nodeFifteen = new Node(15,Colour.PURPLE);
+        Node nodeSixteen = new Node(16,Colour.PURPLE);
+        Node nodeSeventeen = new Node(17,Colour.PURPLE);
+        Node nodeEighteen = new Node(18,Colour.PURPLE);
+        Node nodeNineteen = new Node(19,Colour.PURPLE);
+        Node nodeTwentee = new Node(20,Colour.PURPLE);
+        Node nodeTwenteeOne = new Node(21,Colour.PURPLE);
+        Node nodeTwenteeTwo = new Node(22,Colour.PURPLE);
+        Node nodeTwenteeThree = new Node(23,Colour.PURPLE);
+
+
+    }
 
 }
